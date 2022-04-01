1b7327277ade8b550ec2820bda0a05df8a327d45
Shawn Cannon <scanno16@ford.com>
2022-04-01 10:19:16 -0600

    add 50 pokemon to dex

--- site/index.js
+++ site/index.js
@@ -1,5 +1,5 @@
 const dex = document.querySelector(".pokemon")
-const url = "https://pokeapi.co/api/v2/pokemon/152"
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=151"
 const spinner = document.querySelector(".spinner")
 
 
@@ -14,7 +14,19 @@ fetch(url)
     .then(response => {
         return response.json()
     }).then(parsedResponse => {
-        const imageUrl = parsedResponse.sprites.front_default
-        addImage(imageUrl)
-        spinner.classList.add("hidden")
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(
+            response => response.json()
+        ))
+        console.log(fetches)
+        return Promise.all(fetches)
+            //const imageUrl = parsedResponse.sprites.front_default
+            //addImage(imageUrl)
+
+    }).then(responses => {
+        responses.forEach(response => {
+            spinner.classList.add("hidden")
+            addImage(response.sprites.front_shiny)
+
+        })
     })

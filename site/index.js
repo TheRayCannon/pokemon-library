6b5a8d97f6d439c87e3b309154dcef3e1d032200
Shawn Cannon <scanno16@ford.com>
2022-04-01 10:07:28 -0600

    get one starter pokemon to display on screen

--- site/index.js
+++ site/index.js
@@ -1,3 +1,20 @@
 const dex = document.querySelector(".pokemon")
-const url = "https://pokeapi.co/api/v2/pokemon?limit=20"
+const url = "https://pokeapi.co/api/v2/pokemon/152"
 const spinner = document.querySelector(".spinner")
+
+
+function addImage(url) {
+    const img = document.createElement("img")
+    img.src = url
+    dex.append(img)
+
+}
+
+fetch(url)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const imageUrl = parsedResponse.sprites.front_default
+        addImage(imageUrl)
+        spinner.classList.add("hidden")
+    })

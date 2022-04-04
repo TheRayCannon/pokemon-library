b5a076a1d8a8ce9c4811c47e79e36060859f0283
Shawn Cannon <scanno16@ford.com>
2022-04-04 14:33:37 -0600

    complete formating page entry

--- site/pokemon.js
+++ site/pokemon.js
@@ -7,12 +7,12 @@ const main = document.querySelector("main")
 fetch(`https://pokeapi.co/api/v2/pokemon/${query.get("pokemon")}`)
     .then(response => response.json())
     .then(response => {
-            const name = `${response.name[0]}${response.name.slice(1)}`;
-            const title = document.querySelector("title");
-            title.textContent = name;
-            const pokemonDetails = document.createElement("div")
-            pokemonDetails.classList = "pokemon-details"
-            pokemonDetails.innerHTML = `
+        const name = `${response.name[0].toUpperCase()}${response.name.slice(1)}`;
+        const title = document.querySelector("title");
+        title.textContent = name;
+        const pokemonDetails = document.createElement("div")
+        pokemonDetails.classList = "pokemon-details"
+        pokemonDetails.innerHTML = `
                 <figure>
                     <img src ="${response.sprites.front_shiny}" alt="${name}" />
                     <figcaption>${name}</figcaption>
@@ -20,13 +20,31 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${query.get("pokemon")}`)
 
                 <h2>Abilities</h2>
             `;
-            main.append(pokemonDetails);
-            const abilities = response.abilities
-                .map(response => response.ability.url)
-                .map(url => {
-                    return fetch(url).then(response => response.json())
-                })
-            return Promise.all(abilities)
-        }
-
-    )
+        main.append(pokemonDetails);
+        spinner.classList.add("hidden")
+        console.log(response)
+        const abilityList = response.abilities
+            .map(response => response.ability.url)
+            .map(url => {
+                return fetch(url).then(response => response.json())
+            })
+        return Promise.all(abilityList)
+    }).then(response => {
+        const ul = document.createElement("ul")
+        ul.classList = "abilities"
+        main.append(ul)
+        response.map(response => {
+            const li = document.createElement("li")
+            li.innerHTML = `
+                    <span class="move-name">
+                        ${response.name[0].toUpperCase()}${response.name.slice(1)}
+                    </span>
+                    <span class="move-description">
+                        ${response.effect_entries[1].short_effect}
+                    </span>
+                    `
+            return li;
+        }).forEach(li => {
+            ul.append(li)
+        })
+    })
